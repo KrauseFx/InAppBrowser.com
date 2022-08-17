e5f2c3d26c258a866c838aabbde54c51d54d8b83
Felix Krause <krausefx@gmail.com>
2022-08-17 22:46:41 +0200

    Include code to execute using old and new code

--- ad-container/ad-container/ViewController.m
+++ ad-container/ad-container/ViewController.m
@@ -7,6 +7,7 @@
 
 #import "ViewController.h"
 #import <WebKit/WKWebView.h>
+#import <WebKit/WKContentWorld.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet WKWebView *webView;
@@ -19,6 +20,23 @@
     [super viewDidLoad];
     
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://InAppBrowser.com"] cachePolicy:0 timeoutInterval:0]];
+    
+    dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * 3);
+    dispatch_after(delay, dispatch_get_main_queue(), ^(void){
+        
+        
+        [self.webView evaluateJavaScript:@"document.getElementById('usingNewMethod')"
+                                 inFrame:nil
+                          inContentWorld:[WKContentWorld defaultClientWorld]
+                       completionHandler:^(id _Nullable something, NSError * _Nullable error) {
+            NSLog(@"new: %@", something);
+        }];
+        
+        [self.webView evaluateJavaScript:@"document.getElementById('usingOldMethod')"
+                       completionHandler:^(id _Nullable something, NSError * _Nullable error) {
+            NSLog(@"old: %@", something);
+        }];
+    });
 }
 - (void)viewDidAppear:(BOOL)animated {
     [self.webView reload];
